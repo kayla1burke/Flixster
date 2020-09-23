2ec6a4054b3d0d8cd4a113e64cde5980b5183765
kayla1burke <kayla1.burke@famu.edu>
2020-09-23 18:25:35 -0400

    Part 2

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -27,7 +27,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
-        movieId = jsonObject.getInt("movieId");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
